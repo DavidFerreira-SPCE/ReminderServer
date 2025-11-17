7594c6a5f6cc4906c6a2b075d0bdcb99cb1789e2
David Ferreira <davidleandro670@gmail.com>
2025-11-17 10:11:46 -0300

    Finalizado O Beque

--- src/controllers/lembretesCTRS.js
+++ src/controllers/lembretesCTRS.js
@@ -11,38 +11,49 @@ const listarLembrete = async (_, res) => {
 }
 
 const listarLembreteByMedicamento = async (req, res) => {
-    const { nomeRemedio } = req.body
-    try {
-    const viewName = await pool.query('SELECT * FROM lembretes WHERE nomeRemedio = $1'[nomeRemedio])
-    res.status(200).json(viewName.rows)
- } catch (err) {
-     console.error('Falha na solicitação', err)
-     res.status(500).json({ error: 'Não foi possivel ver os lembretes para este remédio'})
- }
-}
+  const { nomeRemedio } = req.body;
+  
+  try {
+    const viewName = await pool.query(
+      'SELECT * FROM lembretes WHERE nomeRemedio = $1',
+      [nomeRemedio]
+    );
+    
+    res.status(200).json(viewName.rows);
+  } catch (err) {
+    console.error('Falha na solicitação', err);
+    res.status(500).json({ error: 'Não foi possível listar o(s) lembrete(s) desse remédio' });
+  }
+};
 
 
 const criarLembrete = async (req, res) => {
-    const {nomeRemedio,horario,recorrencia} = req.body
+    const { nomeRemedio, horario, recorrencia } = req.body;
     try {
-        const add = await pool.query('INSERT INTO lembretes (nomeRemedio,horario,recorrencia) VALUES ($1, $2, $3) RETURNING *'
-            [nomeRemedio,horario,recorrencia])
-        res.status(200).json(add.rows)
+        const add = await pool.query(
+            'INSERT INTO lembretes (nomeRemedio, horario, recorrencia) VALUES ($1, $2, $3) RETURNING *',
+            [nomeRemedio, horario, recorrencia]
+        );
+        res.status(200).json(add.rows);
     } catch (err) {
-        console.error('Falha na requisição', err)
-        res.status(500).json({ error: 'Verifique as Informações e tente novamente' })
+        console.error('Falha na requisição', err);
+        res.status(500).json({ error: 'Verifique as informações e tente novamente' });
     }
-}
+};
 
 const apagarLembrete = async (req, res) => {
-    const { id } = req.body;
+    const { id } = req.params;
     try {
-        const DELETE = await pool.query('DELETE * FROM lembretes WHERE id = $1' [id])
-        res.status(200).json(DELETE.rows)
+        const DELETE = await pool.query(
+            'DELETE FROM lembretes WHERE id = $1 RETURNING *',
+            [id]
+        );
+        res.status(200).json(DELETE.rows);
     } catch (err) {
-        console.error('Falha na requisição', err)
-        res.status(500).json({ error: 'Não foi possivel apagar o lembrete' })
+        console.error('Falha na requisição', err);
+        res.status(500).json({ error: 'Não foi possível apagar o lembrete' });
     }
 }
 
-module.exports = { listarLembrete,listarLembreteByMedicamento,criarLembrete,apagarLembrete }
+
+module.exports = { listarLembrete, listarLembreteByMedicamento, criarLembrete, apagarLembrete }
